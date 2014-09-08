17fb9b577a1d12fbd982ff8d48da9df55a69f342
Adam Bratin <adam.bratin@gmail.com>
2014-09-08 13:20:32 -0400

    Initial Commit

---
+++ Project2S/Project2S/DataViewController.swift
@@ -0,0 +1,38 @@
+//
+//  DataViewController.swift
+//  Project2S
+//
+//  Created by Adam Bratin on 9/8/14.
+//  Copyright (c) 2014 Brayton. All rights reserved.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+                            
+    @IBOutlet weak var dataLabel: UILabel!
+    var dataObject: AnyObject?
+
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        if let obj: AnyObject = dataObject {
+            self.dataLabel!.text = obj.description
+        } else {
+            self.dataLabel!.text = ""
+        }
+    }
+
+
+}
+
